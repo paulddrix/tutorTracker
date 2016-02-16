cffc378417f68a5bf5ec56d843a9e4117239ff0e
Paul Diederichs <pauldiederichs95@gmail.com>
2016-02-16 00:35:51 -0500

    close #6

--- public/js/tutorApp.js
+++ public/js/tutorApp.js
@@ -1,7 +1,9 @@
 (function(){
 	console.log("tutorApp loaded *****");
+	//Dashboard page
 	//search course when user clicks button
 	if (document.getElementById('searchCourses')) {
+
 		document.getElementById('searchCourses').addEventListener('click',function() {
 
 			var searchQuery = document.getElementById('courseName').value;
@@ -13,13 +15,19 @@
 			    if (xhttp.readyState == 4 && xhttp.status == 200) {
 						console.log(xhttp);
 						var response = JSON.parse(xhttp.responseText);
-						var resultsHtml ='<ul class="userLi">';
+						var resultsHtml ='<div class="listCont">';
 						for (var i = 0; i < response.length; i++) {
-							resultsHtml+= "<li><b>Name</b>&nbsp;"+response[i]['firstName']+" "+ response[i]['lastName'] +" "+ " <b>Total Hours </b>" +response[i]['monthlyTotalHours'] +"</li>";
+						resultsHtml+= "<div class=\"userLi\"><b>Name</b>&nbsp;"+response[i]['firstName']+" "+ response[i]['lastName'] +" "+ " <b>Total Hours </b>" +response[i]['monthlyTotalHours'] +"<button id=\"dynamicAssign\" data-tutor-name=\""+response[i]['firstName']+" "+response[i]['lastName']+ "\" data-tutor-id=\""+response[i]['userId']+ " \">Assign</button></div>";
 						}
-						resultsHtml+= "</ul>";
+						resultsHtml+= "</div>";
 
 			      document.getElementById("courseSearchRes").innerHTML =resultsHtml;
+						document.getElementById('dynamicAssign').addEventListener('click',function() {
+							var tutorName = this.getAttribute("data-tutor-name");
+							var tutorId = this.getAttribute("data-tutor-id");
+							document.getElementsByClassName("assignTutor").assignTutor[0].innerHTML =tutorName;
+							document.getElementsByClassName("assignTutor").assignTutor[0].value =tutorId;
+						});
 			    }
 			  };
 			  xhttp.open("GET", "/tutoreligibility/"+searchQuery, true);
@@ -32,14 +40,9 @@
 	//document.getElementById("wrapper").style.backgroundColor = "red";
 	// ACTIVE NAV
 	var nav = document.getElementsByTagName("NAV");
-	console.log(nav);
   var anchor = nav[0]['children'];
-	console.log(anchor);
       current = window.location.pathname.split('/')[1];
-			console.log(current);
       for (var i = 0; i < anchor.length; i++) {
-				console.log("anchor sub "+anchor[i].href.split('/')[3]);
-				console.log("current "+current);
       if(anchor[i].href.split('/')[3] == current) {
           anchor[i].className = "activeNav";
       }
